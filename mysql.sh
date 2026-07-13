979818b09a97726885171b468ea1ccd7cf441cc0
gautami4 <gowthami.it4@gmail.com>
2026-07-13 14:36:17 +0530

    mysql installation

--- mysql.sh
+++ mysql.sh
@@ -2,20 +2,20 @@
 
 USERIDD=$( id -u )
 
-if ( $USERIDD -ne 0 )
+if [ $USERIDD -ne 0 ]
 then
     echo "you should have root access to run this script"
 fi
 
-if ( $USERIDD -eq 0 )
+if [ $USERIDD -eq 0 ]
 then
     dnf list installed mysql
-    if ( $? -eq 0 )
+    if [ $? -eq 0 ]
     then
         echo "mysql already installed"
     else
         dnf install mysql -y
-        if ( $? -eq 0 )
+        if [ $? -eq 0 ]
         then
             echo "Mysql installation success"
         else

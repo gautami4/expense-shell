a31db45cd12259f0cf66d2ca6ba4627a59b91ca9
gautami4 <gowthami.it4@gmail.com>
2026-07-13 14:42:18 +0530

    mysql installation

--- mysql.sh
+++ mysql.sh
@@ -2,6 +2,15 @@
 
 USERIDD=$( id -u )
 
+VALIDATE(){
+    if [ $1 -eq 0 ]
+        then
+            echo "$2 installation success"
+        else
+            echo "$2 installation Failure"
+        fi
+}
+
 if [ $USERIDD -ne 0 ]
 then
     echo "you should have root access to run this script"
@@ -9,18 +18,14 @@ fi
 
 if [ $USERIDD -eq 0 ]
 then
-    dnf list installed mysql
+    dnf list installed nginx
     if [ $? -eq 0 ]
     then
         echo "mysql already installed"
     else
-        dnf install mysql -y
-        if [ $? -eq 0 ]
-        then
-            echo "Mysql installation success"
-        else
-            echo "Mysql installation Failure"
-        fi
+        dnf install nginx -y
+        VALIDATE $? "nginx"
+        
     fi
 
 fi                          

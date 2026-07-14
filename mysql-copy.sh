9946193769c8d99cad53c646a9f1a5a89fd8e49f
gautami4 <gowthami.it4@gmail.com>
2026-07-14 10:53:08 +0530

    backend server installation

--- mysql-copy.sh
+++ mysql-copy.sh
@@ -45,7 +45,7 @@ VALIDATE $? "Starting mysql server"
 mysql_secure_installation --set-root-pass ExpenseApp@1
 VALIDATE $? "setting root password"
 
-mysql -h 172.31.18.136 -u root -pExpenseApp@1 &>>$LOG_FILE_NAME
+mysql -h 98.88.248.138 -u root -pExpenseApp@1 &>>$LOG_FILE_NAME
 
 
 
